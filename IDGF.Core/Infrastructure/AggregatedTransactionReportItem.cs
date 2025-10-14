ac79528f9718bdf9c7e4b1f2358ec7ae8c3fcc0b
alirezafsch <alireza.fasooniehchi@gmail.com>
2025-10-14 12:39:08 +0330

    make the invesmentPice nullable

--- IDGF.Core/Infrastructure/AggregatedTransactionReportItem.cs
+++ IDGF.Core/Infrastructure/AggregatedTransactionReportItem.cs
@@ -5,7 +5,7 @@
         public string Symbol { get; set; }
         public DateOnly DateOfMaturity { get; set; }
         public decimal Quantity { get; set; }
-        public decimal TotalPurchasePrice { get; set; } 
+        public decimal? TotalPurchasePrice { get; set; } 
         public decimal TotalFaceValue { get; set; }
     }
 }
